68293c8263a9cb4fae34411397cad94fc3da2af4
Sohel Mondal <mondalsohel47025@gmail.com>
2026-04-14 12:44:58 +0530

    Bug Fixed

--- src/components/ui/globe.tsx
+++ src/components/ui/globe.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useRef, useState } from "react";
-import { Color, Scene, Fog, PerspectiveCamera, Vector3 } from "three";
+import { Color, Scene, Fog, PerspectiveCamera, Vector3, Group } from "three";
 import ThreeGlobe from "three-globe";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
@@ -53,7 +53,7 @@ interface WorldProps {
 
 export function Globe({ globeConfig, data }: WorldProps) {
   const globeRef = useRef<ThreeGlobe | null>(null);
-  const groupRef = useRef<THREE.Group>(null!);
+  const groupRef = useRef<Group>(null!);
   const [isInitialized, setIsInitialized] = useState(false);
 
   const defaultProps = {
